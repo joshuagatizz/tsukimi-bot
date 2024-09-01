554bf79b90a4c8c1f2152126871128082ddd0397
Tsaqif Ammar <tsaqifammarmuh@gmail.com>
2024-09-01 15:24:51 +0700

    Add loading bot token via .env

--- app.go
+++ app.go
@@ -1,8 +1,19 @@
 package main
 
-import bot "tsukimi-web/bot"
+import (
+	"log"
+	"os"
+	bot "tsukimi-web/bot"
+
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	bot.BotToken = "PLACEHOLDER"
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	bot.BotToken = os.Getenv("BOT_TOKEN")
 	bot.Run() // call the run function of bot/bot.go
 }
